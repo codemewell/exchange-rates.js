7e72f14cfec8aa56057ccc24c7ac3ab795739b45
codemewell <17128190+codemewell@users.noreply.github.com>
2022-03-28 10:36:37 +0200

    [tweak] code refactor

--- exchange-rates.js
+++ exchange-rates.js
@@ -1,31 +1,50 @@
-const url = 'https://api.frankfurter.app/latest'
-const req = new Request(url)
-const res = await req.loadJSON()
-const amount = res.amount;
-const base = res.base;
-const date = res.date;
-const rates = res.rates.USD;
-console.log(rates)
+class CurrencyData {
 
-const i = new Request("https://raw.githubusercontent.com/transferwise/currency-flags/master/src/flags/eur.png");
-const img = await i.loadImage()
+    constructor(base = "EUR") {
+        this.base = base;
+    }
 
-let i2 = null;
-let img2 = null;
-i2 = new Request("https://raw.githubusercontent.com/transferwise/currency-flags/master/src/flags/usd.png");
-img2 = await i2.loadImage()
+    async fetch() {
+        const url = `https://api.frankfurter.app/latest?from=${this.base}`;
+        const req = new Request(url);
+        const res = await req.loadJSON();
+        this.amount = res.amount;
+        this.base = res.base;
+        this.date = res.date;
+        this.rates = res.rates;
+    }
+}
 
-const rates3 = res.rates.CHF;
-let i3 = null;
-let img3 = null;
-i3 = new Request("https://raw.githubusercontent.com/transferwise/currency-flags/master/src/flags/chf.png");
-img3 = await i3.loadImage()
+async function displayCurrencies({ currenciesToDisplay = ['USD', 'CHF', 'GBP'], currencySourceData, displayStack } = {}) {
 
-const rates4 = res.rates.GBP;
-let i4 = null;
-let img4 = null;
-i4 = new Request("https://raw.githubusercontent.com/transferwise/currency-flags/master/src/flags/gbp.png");
-img4 = await i4.loadImage()
+    async function _loadFlagImage(currencySymbol) {
+        const lowerCasedCurrencySymbol = currencySymbol.toLowerCase();
+        const i = new Request(`https://raw.githubusercontent.com/transferwise/currency-flags/master/src/flags/${lowerCasedCurrencySymbol}.png`);
+        return i.loadImage()
+    }
+
+    function _displayCurrencyRateItem(flagImage, rate, symbol) {
+        let newStack = displayStack.addStack();
+
+        newStack.layoutHorizontally();
+        newStack.addText(rate + ' ' + symbol);
+        newStack.addSpacer(4);
+
+        let image = newStack.addImage(flagImage);
+        image.imageSize = new Size(20, 20);
+
+        newStack.addSpacer(4)
+    }
+
+    currenciesToDisplay.forEach((currencySymbol) => {
+        let currencyFlagImage = await _loadFlagImage(currencySymbol);
+        let currencyRate = (1 / currencySourceData[currencySymbol]).toPrecision(2);
+        _displayCurrencyRateItem(currencyFlagImage, currencyRate, currencySymbol);
+    })
+
+};
+
+const currencyData = await new CurrencyData().fetch();
 
 let widget = createWidget(amount)
 if (config.runsInWidget) {
@@ -51,43 +70,7 @@ function createWidget(amount) {
 
     upperStack.addSpacer(10)
 
-    let baseStack = upperStack.addStack();
-    baseStack.layoutHorizontally()
-
-    let amountText = baseStack.addText(amount + ' ' + base)
-    baseStack.addSpacer(4)
-    let image = baseStack.addImage(img)
-    image.imageSize = new Size(20, 20)
-
-    upperStack.addSpacer(4)
-
-    let USDStack = upperStack.addStack();
-    USDStack.layoutHorizontally()
-
-    let firstCurrency = USDStack.addText(rates + ' USD')
-    USDStack.addSpacer(4)
-    let image2 = USDStack.addImage(img2)
-    image2.imageSize = new Size(20, 20)
-
-    upperStack.addSpacer(4)
-
-    let CHFStack = upperStack.addStack();
-    CHFStack.layoutHorizontally()
-
-    let secondCurrency = CHFStack.addText(rates3 + ' CHF')
-    CHFStack.addSpacer(4)
-    let image3 = CHFStack.addImage(img3)
-    image3.imageSize = new Size(20, 20)
-
-    upperStack.addSpacer(4)
-
-    let GBPStack = upperStack.addStack();
-    GBPStack.layoutHorizontally()
-
-    let thirdCurrency = GBPStack.addText(rates4 + ' GBP')
-    GBPStack.addSpacer(4)
-    let image4 = GBPStack.addImage(img4)
-    image4.imageSize = new Size(20, 20)
+    displayCurrencies({ currencySourceData: currencyData, displayStack: upperStack });
 
     return w
 }
